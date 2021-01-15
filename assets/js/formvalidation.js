d7af53bd24c326583ceb5e8de032bef83a4cf0cb
PriyankaMuthuluri <priyanka@insiconnect.in>
2021-01-15 15:56:44 +0530

    about and login pages

--- assets/js/formvalidation.js
+++ assets/js/formvalidation.js
@@ -1,20 +1,21 @@
 function login()
 	{
-		var uname = document.getElementById("email").value;
+		var email = document.getElementById("email").value;
 		var pwd = document.getElementById("pws").value;
 		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-		if(uname =='')
+		if(email =='')
 		{
-			alert("please enter user name.");
+			alert("please enter email.");
+		}
+        else if(!filter.test(email))
+		{
+			alert("Enter valid email id.");
 		}
 		else if(pwd=='')
 		{
         	alert("enter the password");
 		}
-		else if(!filter.test(uname))
-		{
-			alert("Enter valid email id.");
-		}
+
 		else if(pwd.length >=8 || pwd.length < 16)
 		{
 			alert("Enter a Correct Password.");
@@ -22,36 +23,35 @@ function login()
 		else
 		{
 	alert('Your Logged In');
-			}
+        }
     }
     function signup()
 	{
-		var uname = document.getElementById("inputEmail").value;
+		var inemail = document.getElementById("inputEmail").value;
         var pwds = document.getElementById("inputPws").value;
         var confirmpwd = document.getElementById("inputConfirmPws").value;
         var ucountry = document.getElementById("countries").value;
         
-		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		var filtere = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 		var pwsfilter = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})$/;
-		if(uname =='')
+		if(inemail =='')
 		{
-			alert("please enter user name.");
+			alert("please enter email id.");
+		}
+        else if(!filtere.test(inemail))
+		{
+			alert("Enter valid email id.");
 		}
 		else if(pwds=='')
 		{
         	alert("enter the password");
 		}
-		else if(!filter.test(uname))
-		{
-			alert("Enter valid email id.");
-		}
+
 		else if(!pwsfilter.test(pwds)||(pwds.length < 8 || pwds.length > 15))
 		{
 			alert("Give a strong pasword ");
         }
-        else if(confirmpwd != pwds){
-            alert("Password dosen't matched");
-        }
+
         else if(ucountry == '')
         {
             alert("Please specify your country");
@@ -59,5 +59,5 @@ function login()
 		else
 		{
 	alert('Now Your a Number of INSI');
-			}
+}
 	}
